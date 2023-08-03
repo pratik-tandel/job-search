f8774e7f677312dac69efb313aa72899ce574bf9
PratikTandel <pratik.tandel@1rivet.com>
2023-08-03 15:58:50 +0530

    feat: add Home Page Layout

---
+++ app/_layout.js
@@ -0,0 +1,16 @@
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+
+export default Layout = () => {
+    const [fontsLoaded] = useFonts({
+        DMBold: require('../assets/fonts/DMSans-Bold.ttf'),
+        DMMedium: require('../assets/fonts/DMSans-Medium.ttf'),
+        DMRegular: require('../assets/fonts/DMSans-Regular.ttf')
+    });
+
+    if (!fontsLoaded) {
+        return null;
+    }
+
+    return <Stack />;
+}
---
+++ app/index.js
@@ -0,0 +1,7 @@
+import { Redirect } from "expo-router";
+
+const Home = () => {
+    return <Redirect href="/home" />;
+}
+
+export default Home;
